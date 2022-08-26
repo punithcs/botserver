68c7f6609866adf496657bec135fd4d0cfff44ce
Punith C S <pucs@cisco.com>
2022-08-26 15:01:13 +0530

    index.js

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World! Punith');
-}).listen(3000, ()=>{
-    console.log('server is runing at port 3000')
-});
