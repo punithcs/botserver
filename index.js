6835cd126e7c78561f968b01577e519018c5c3b8
Punith C S <pucs@cisco.com>
2022-08-26 15:49:35 +0530

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Request");
-    console.log("End of Request");
+    console.log("Request......");
+    console.log("End of Request...............");
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
