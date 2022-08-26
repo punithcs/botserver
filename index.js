dbda114df53fb82782022ef58ff14bae302285a9
Punith C S <pucs@cisco.com>
2022-08-26 15:29:25 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Request");
-    console.log(res);
     console.log("End of Request");
     res.send('Yo!')
 })
