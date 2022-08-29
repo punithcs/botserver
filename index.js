1f17b497874c80b689eb8f1a6beaa13c76e57a3d
Punith C S <pucs@cisco.com>
2022-08-29 11:54:08 +0530

    Setting Header

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express()
 app.all('/', (req, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.setHeader('Set-Cookie', ['type=ninja', 'language=javascript']);
-    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.writeHead(200, { 'Content-Type': 'application/json' });\
+    console.log(req.body);
     response.end('ok');
     console.log(response);
     console.log("Request......");
     console.log("End of Request...............");
-    console.log(req);
 })
 app.listen(process.env.PORT || 3000)
